8e8436bcd68f2e953a3d0dbc7c349e41bd2f029e
cahit.evmek <cahitevmek@yahoo.com>
2020-04-24 03:06:54 +0300

    write, click, control methodları eklendi

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -6,12 +6,11 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,11 +19,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  */
 public class AppTest
 {
-    public WebDriver driver = null;
-    public String baseUrl = "https://www.hepsiburada.com/";
+    private WebDriver driver = null;
+    //private String baseUrl = "https://www.hepsiburada.com/";
+    private String baseUrl = "https://www.n11.com/";
+    private WebDriverWait wait = null;
 
     @Before
     public void setUp() {
+
         System.setProperty("webdriver.chrome.driver","C:\\Users\\cahit.evmek\\Desktop\\HepsiBuradaCases\\Driver\\chromedriver.exe");
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         ChromeOptions options = new ChromeOptions();
@@ -35,21 +37,45 @@ public class AppTest
         capabilities.setCapability("chrome.binary","C:\\Users\\cahit.evmek\\Desktop\\SelleniumProject\\src\\library\\drivers\\chromedriver.exe");
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         driver = new ChromeDriver(capabilities);
-
+        wait = new WebDriverWait(driver,10);
     }
 
     @Test
     public void correctOpen() throws InterruptedException {
+
         driver.get(baseUrl);
-
-
-        //Açık Bekleme Tipi olan WebdriverWait Tanımlanır.
-        //Bu, bir TimeoutException özel durumu oluşturmadan önce 15 saniye kadar bekler veya öğenin bulduğu takdirde 0 - 15 saniye içinde döndürür
-        //WebDriverWait wait = new WebDriverWait(driver,TimeOut);
-        WebDriverWait wait = new WebDriverWait(driver,10);
 
         Thread.sleep(5000);
 
+        click("//*[@id=\"userKvkkModal\"]/div/div[2]/span");
+        Thread.sleep(5000);
+        textControlByXpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/div[2]/div/div/a[1]","Giriş Yap");
+        textControlByClassName("btnSignIn","Giriş Yap");
+      //  click("//*[@id=\"header\"]/div/div/div[2]/div[2]/div[2]/div/div/a[1]");
+
+        //*[@id="header"]/div/div/div[2]/div[2]/div[2]/div/div/a[1]
+
+
+        //hepsi burada
+//        textControlByXpath("//*[@id=\"myAccount\"]/span/a/span[2]","Giriş Yap");
+
+
+
+        //click("//*[@id=\"myAccount\"]");
+        //click("//*[@id=\"login\"]");
+       // write("//*[@id=\"email\"]","cahitevmek@yahoo.com");
+       // write("//*[@id=\"password\"]","");
+
+     //   Assert.assertEquals(driver.findElement(By.className("btnSignIn")).getAttribute(), "Google");
+
+       // click("//*[@id=\"login-container\"]/section[1]/div/div[2]/div[1]/div/label");
+      //  click("//*[@id=\"loginButton\"]");
+
+    //    WebElement kvkk = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//*[@id=\"userKvkkModal\"]/div/div[2]/span"))));
+    //    kvkk.click();
+
+
+/*
         WebElement myAccount = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//*[@id=\"myAccount\"]"))));
         myAccount.click();
 
@@ -102,5 +128,37 @@ public class AppTest
     public void tearDown() {
         //      driver.quit();
     }
+
+    public void click(String Xpath) throws InterruptedException {
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Xpath)));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(Xpath)));
+        Thread.sleep(500);
+        this.driver.findElement(By.xpath(Xpath)).click();
+    }
+
+    public void write(String Xpath, String text) throws InterruptedException {
+
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Xpath)));
+        this.driver.findElement(By.xpath(Xpath)).clear();
+        Thread.sleep(500);
+        this.driver.findElement(By.xpath(Xpath)).sendKeys(new CharSequence[]{text});
+        this.driver.findElement(By.xpath(Xpath)).sendKeys(new CharSequence[]{Keys.TAB});
+    }
+
+    public void textControlByClassName(String classname, String text) throws InterruptedException {
+
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(classname)));
+        Thread.sleep(500);
+        Assert.assertEquals(driver.findElement(By.className(classname)).getText(), text);
+    }
+
+    private void textControlByXpath(String Xpath, String text) throws InterruptedException{
+
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Xpath)));
+        Thread.sleep(500);
+        Assert.assertEquals(driver.findElement(By.xpath(Xpath)).getText(), text);
+    }
+
 }
 
